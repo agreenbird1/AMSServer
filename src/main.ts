8db60752efddcb25c28b2cc00777b1d68e972d94
afanti <afanti@juexiaotime.com>
2023-04-09 22:49:13 +0800

    chore: validate dto 参数校验

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { TransformInterceptor } from './common/interceptors/transform.intercepto
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 import { generateDocument } from './doc';
+import { ValidationPipe } from '@nestjs/common';
 import * as session from 'express-session';
 
 async function bootstrap() {
@@ -13,6 +14,8 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
   // 异常过滤器
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+  // validate 参数格式 => dto 校验
+  app.useGlobalPipes(new ValidationPipe());
   generateDocument(app);
   await app.listen(3000);
 }
