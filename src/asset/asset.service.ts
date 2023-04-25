130fa801ee169b5ce79236482fca7c8ea1cb9a91
afanti <afanti@juexiaotime.com>
2023-04-25 22:52:44 +0800

    fix: save方法需传入实体

--- src/asset/asset.service.ts
+++ src/asset/asset.service.ts
@@ -18,6 +18,11 @@ export class AssetService {
 
   async create(createAssetDto: CreateAssetDto) {
     const asset = new Asset();
+    asset.name = createAssetDto.name;
+    asset.specification = createAssetDto.specification;
+    asset.quantity = createAssetDto.quantity;
+    asset.amount = createAssetDto.amount;
+    asset.location = createAssetDto.location;
     asset.picture =
       createAssetDto.picture ||
       'https://ydassetpicture.oss-cn-beijing.aliyuncs.com/pc-employee/app/dist/1681985422290/img/no-pic.2c7f0ca6.png';
@@ -30,10 +35,7 @@ export class AssetService {
     const handleUser = await this.asset.manager.getRepository(User).findOneBy({
       id: createAssetDto.userId,
     });
-    const newAsset = await this.asset.manager.save({
-      ...createAssetDto,
-      ...asset,
-    });
+    const newAsset = await this.asset.manager.save(asset);
     // 新增资产需要同步到监控表
     this.asset.manager.getRepository(Monitor).save({
       type: 1,
