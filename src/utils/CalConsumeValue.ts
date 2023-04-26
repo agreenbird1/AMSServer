ad9e633c1fbe551d9071e7ba6d2dfbb129cdd0fb
afanti <afanti@juexiaotime.com>
2023-04-26 23:20:29 +0800

    fix ts中dayjs引入方式

--- src/utils/CalConsumeValue.ts
+++ src/utils/CalConsumeValue.ts
@@ -1,7 +1,11 @@
-import dayjs from 'dayjs';
+// ts 中不可直接导入
+import * as dayjs from 'dayjs';
 
-export default (startTime: Date, value: number) => {
-  const diffDays = dayjs(new Date()).diff(dayjs(startTime), 'days');
+export default (startTime: Date | string, value: number) => {
+  const diffDays = dayjs(dayjs(new Date()).format('YYYY-MM-DD HH:mm:ss')).diff(
+    startTime,
+    'days',
+  );
   // 每年使用时候价值损耗 0.35
   const depreValue = (0.35 * value * diffDays) / 365;
 
