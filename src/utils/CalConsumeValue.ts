ab41da951f7291fb566bd3c1a7b6535b80ad8e57
afanti <afanti@juexiaotime.com>
2023-05-21 18:20:06 +0800

    fix: depreValue

--- src/utils/CalConsumeValue.ts
+++ src/utils/CalConsumeValue.ts
@@ -6,8 +6,19 @@ export default (startTime: Date | string, value: number) => {
     startTime,
     'days',
   );
-  // 每年使用时候价值损耗 0.35
-  const depreValue = (0.35 * value * diffDays) / 365;
 
-  return value - depreValue;
+  const nMultiply = (n, x) => {
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+      result *= x;
+    }
+    return result;
+  };
+
+  const diffYears = Math.floor(diffDays / 365);
+  const yearValue = nMultiply(diffYears, 0.65) * value;
+  // 每年使用时候价值损耗 0.35
+  const depreValue = (1 - 0.35 * ((diffDays % 365) / 365)) * yearValue;
+
+  return depreValue;
 };
